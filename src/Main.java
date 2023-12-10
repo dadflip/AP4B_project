5e92d978ac9559120a9f866818a3866703dd76b9
axela <axel.antunes@utbm.fr>
2023-12-11 00:44:55 +0100

    git add . git commit -m "Enhance UI: Display player hands in a 2x4 grid with unique colors
    - Adjusted layout to display cards evenly in a 2x4 grid within the frame
    - Assigned a unique background color for each player's hand based on their index
    - Changed card buttons to be initially non-clickable, with ActionListener handling future interactions"

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,28 @@
 import main.java.com.utmunchkin.Rules;
+import main.java.com.utmunchkin.gameplay.Play;
 import main.java.com.utmunchkin.players.ListOfPlayer;
 import main.java.com.utmunchkin.players.Player;
 import main.java.com.utmunchkin.utils.DiceRoller;
-
 public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
-        DiceRoller dice = new DiceRoller();
+        //DiceRoller dice = new DiceRoller(); /*unused for the moment, used in setTurn Method */
+
         Rules rules = new Rules();
         ListOfPlayer list = new ListOfPlayer();
-        list.addPlayer(new Player("Player 1", 1));
-        list.addPlayer(new Player("Player 2", 2));
-        list.addPlayer(new Player("Player 3", 3));
-        list.addPlayer(new Player("Player 4", 4));
+        list.addPlayer(new Player("Joueur 1", 1));
+        list.addPlayer(new Player("Joueur 2", 2));
+        list.addPlayer(new Player("Joueur 3", 3));
+        list.addPlayer(new Player("Joueur 4", 4));
         rules.setTurn(list);
+
+
+        // Now you have the first player index from Rules
+        int firstPlayerIndex = rules.getFirstPlayerIndex();
+
+        // Create an instance of Play with the first player index
+        Play game = new Play(list, firstPlayerIndex);
+        game.gameProcess();
+
     }
 }
